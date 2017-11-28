0f0a48884d1b730dbfe7a27be76cb4d20203a93e
yokiyokiyoki <youjie@hudongpai.com>
2017-11-28 15:41:22 +0800

    提交第三个页面

--- src/components/instance/instance.vue
+++ src/components/instance/instance.vue
@@ -13,9 +13,17 @@
         };
       },
       created() {
-        //mess和message指向的是同一个内存
+        //mess和message（当前实例的数据）指向的是同一个内存
         mess.name = "12";
         console.log(this.message === mess);
+        //mess={name:'vue'} 没有改变当前实例，因为这样相当于开辟了新的内存（新的对象），但是message还是指向的是原来的
+
+        //data准备好,但是尚未挂载到dom节点。而且里面的属性，this直接加个$等同
+        console.log(this.$data, this.$el === undefined);
+      },
+      mounted() {
+        // 实例已经挂载到dom节点
+        console.log(this.$el);
       }
     };
 </script>
