6fbb62fc3138ca85f5628a5b43aa9c9c04623fca
yokiyokiyoki <youjie@hudongpai.com>
2017-11-28 21:01:00 +0800

    更新for循环

--- src/components/for/for.vue
+++ src/components/for/for.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="for">
       <!-- key是为了保持这个dom元素唯一，因为vue会缓存 -->
-      <!-- item和index位置不可换 -->
+      <!-- item和index位置不可换,index可选 -->
       <span v-for="(item,index) in list" :key="item">类目是{{item}}，索引是{{index}}</br></span>
+        <!-- 可以循环一个template，一个整体 -->
+      <template v-for="(item,index) in list">
+          <span>我是整体里面的{{item}}</span>
+          <span>我是整体里面的{{index}}</span>
+        </template> 
   </div>
 </template>
 <script>
