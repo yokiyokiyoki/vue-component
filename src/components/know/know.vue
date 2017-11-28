698ff5d6b7931239370514a096a5daf27143ff2e
yokiyokiyoki <youjie@hudongpai.com>
2017-11-28 14:41:47 +0800

    上传页面

--- src/components/know/know.vue
+++ src/components/know/know.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="know">
-        <div>1.输出的是字符串：{{rawHtml}}</div>
-        <div>2.用v-html指令：<span v-html="rawHtml"></span></div>
-        <div>3.{{name}}</div>
+        <!-- 双括号文本差值数据绑定 -->
+        <!-- 但是输入的只能是字符串，想要原样输出html，必须用指令v-html -->
+        <div>1.rawHtml输出的是字符串：{{rawHtml}}</div>
+        <div>2.rawHtml用v-html指令：<span v-html="rawHtml"></span></div>
+        <div>3.name：{{name}}</div>
         <div>
-            4.v-model可以双向绑定
+            4.v-model可以双向绑定，绑定的是name
             <input type="text" v-model="name">
         </div>
   </div>
