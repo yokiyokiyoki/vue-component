43df12d1593b53af4bb3a325f3b07e7decc1aa4c
youjie <youjie@hudongpai.com>
2017-11-30 21:34:59 +0800

    提交页面

--- src/components/bind/bind.vue
+++ src/components/bind/bind.vue
@@ -7,6 +7,10 @@
     <button v-on:click="handler">按钮点击事件</button>
     <!-- 监听事件一般写成下面这样 -->
     <button @click="handler">按钮点击事件</button>
+    <!-- 如果想要传递参数 -->
+    <button @click="handlerArg('传递了参数')">传递了参数的点击事件</button>
+    <!-- 如果又想要传递参数，又想要获得原生事件 -->
+    <button @click="handlerArgAndEvent('传递了参数和原生事件', $event)">传递了参数和原生事件的点击事件</button>
   </div>
 </template>
 <script>
@@ -19,7 +23,17 @@
     methods: {
       handler() {
         alert(11);
+        //也可以直接写handler（event）,没有event，如果上面没有传参数，默认参数是事件
+        console.log(event)
+      },
+      handlerArg(arg) {
+        // 也能写到event,但是不推荐,因为不像下面那样event想怎么写就怎么写
+        console.log(arg, event)
+      },
+      handlerArgAndEvent(arg, e) {
+        console.log(arg, e)
       }
     }
   };
+
 </script>
