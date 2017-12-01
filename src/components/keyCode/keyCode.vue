141ba4979072d895516e9083802a5c7b6e5fcdb1
youjie <youjie@hudongpai.com>
2017-12-01 23:44:24 +0800

    提交退格和删除

--- src/components/keyCode/keyCode.vue
+++ src/components/keyCode/keyCode.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="keyCode">
-      <input type="text" placeholder="回车按键修饰符13" @keyup.13="handler">
+      <input type="text" placeholder="回车按键修饰符13" @keyup.13="handlerEnter">
       <!-- 但是我们肯定记不住所有keyCode（13是keycode），所以vue提供了别名 -->
-      <input type="text" placeholder="回车按键修饰符enter" @keyup.enter="handler">
+      <input type="text" placeholder="回车按键修饰符enter" @keyup.enter="handlerEnter">
+      <input type="text" placeholder="退格和删除按键修饰符" @keyup.delete="handlerDelete">
   </div>
 </template>
 <script>
@@ -11,8 +12,11 @@
         return {};
       },
       methods: {
-        handler() {
+        handlerEnter() {
           console.log("回车事件");
+        },
+        handlerDelete() {
+          console.log("退格和删除事件");
         }
       }
     };
