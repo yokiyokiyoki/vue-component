fa06a567bff5b824f0fec3d5b598b2c88a6a5c08
youjie <youjie@hudongpai.com>
2017-12-01 23:30:55 +0800

    提交按键页面

--- src/components/keyCode/keyCode.vue
+++ src/components/keyCode/keyCode.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="keyCode"></div>
+  <div class="keyCode">
+      <input type="text" placeholder="回车按键修饰符13" @keyup.13="handler">
+      <!-- 但是我们肯定记不住所有keyCode（13是keycode），所以vue提供了别名 -->
+      <input type="text" placeholder="回车按键修饰符enter" @keyup.enter="handler">
+  </div>
 </template>
 <script>
     export default {
       data() {
         return {};
+      },
+      methods: {
+        handler() {
+          console.log("回车事件");
+        }
       }
     };
 </script>
