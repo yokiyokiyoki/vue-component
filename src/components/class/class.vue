334cb6413be21e867d141906e3bd17ff6ec509a8
youjie <youjie@hudongpai.com>
2017-12-04 22:08:16 +0800

    提交class页面

--- src/components/class/class.vue
+++ src/components/class/class.vue
@@ -2,17 +2,31 @@
   <div class="class">
       <button @click="changeClass">改变类名</button>
       <!-- v-bind:class指令可以和普通class共存 -->
-      <div class="bold" :class="{green:true}">动态绑定绿色类名</div>
-      <div class="bold" :class="{size:active}">动态绑定绿色类名</div>
-      <div class="bold" :class="classObj">computed动态绑定绿色类名</div>
+      <div class="bold" :class="{green:true}">对象动态绑定绿色类名</div>
+      <div class="bold" :class="{size:active}">对象动态绑定绿色类名</div>
+      <div class="bold" :class="classObj">对象computed动态绑定类名</div>
       <!-- 除了使用对象,还可以使用数组 -->
+      <div :class="[greenClass,boldClass]">数组动态绑定类名</div>
+      <!-- 可以用三元表达式 -->
+      <div :class="[active?redClass:greenClass,boldClass]">数组动态绑定类名</div>
+      <!-- 但是如果有很多判断条件的时候，三元表达式就会太繁琐，可以使用数组里面对象的形式来写 -->
+      <div :class="[{red:active},boldClass]">数组动态绑定类名</div>
   </div>
 </template>
 <script>
     export default {
       data() {
         return {
-          active: false
+          active: false,
+          greenClass: {
+            green: "green"
+          },
+          boldClass: {
+            bold: "bold"
+          },
+          redClass: {
+            red: "red"
+          }
         };
       },
       methods: {
