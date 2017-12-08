632bef7ef14ebd468702b447a3cdd99ede27faa0
youjie <youjie@hudongpai.com>
2017-12-08 15:22:05 +0800

    保存一下文本输入内容

--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="form"></div>
+  <div class="form">
+    <!-- 文本 -->
+    <input type="text" v-model="message" placeholder="单个文本">
+    <p>input文本输入:{{message}}</p>
+  </div>
 </template>
 <script>
-    export default {
-      data() {
-        return {};
-      }
-    };
+  export default {
+    data() {
+      return {
+        message: ""
+      };
+    }
+  };
 </script>
 
