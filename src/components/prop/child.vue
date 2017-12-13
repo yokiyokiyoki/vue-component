8a0a6cf07141fe0ba633c7e4110c92f0aa604644
youjie <youjie@hudongpai.com>
2017-12-13 19:44:43 +0800

    提交prop

--- src/components/prop/child.vue
+++ src/components/prop/child.vue
@@ -2,6 +2,10 @@
   <div class="child">
       <p>我是子组件</p>
       <p>{{myMessage}}</p>
+      <p>{{myNum}}</p>
+      <input type="text" v-model="message">
+      <p>{{computedNum}}</p>
+      <p>{{myOther}}</p>
   </div>
 </template>
 <script>
@@ -9,7 +13,31 @@
       name: "child",
       props: {
         //   子组件接收，必须以驼峰命名法
-        myMessage: String
+        myMessage: String,
+        myNum: Number,
+        myOther: [String, Boolean], //可以是string类型，或者布尔类型
+        defaultNum: {
+          required: false, //必须不必须
+          default: 200 //默认为200
+        },
+        defaultObj: {
+          required: false,
+          // 对象/数组的默认值一定是一个函数
+          default() {
+            return {};
+          }
+        }
+      },
+      //   子组件不能修改父组件传过来的数据,但是我们可以通过data来作为局部数据使用，或者使用computed计算出来一个便利的值来使用
+      data() {
+        return {
+          message: this.myMessage //作为局部数据来使用，可以在组件内更改这个message，比如input
+        };
+      },
+      computed: {
+        computedNum() {
+          return this.defaultNum + 1;
+        }
       }
     };
 </script>
--- src/components/prop/prop.vue
+++ src/components/prop/prop.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="prop">
-      <!-- 字面量语法，kebab-case写法 -->
+      <p>kebab-case写法</p>
+      <!-- 字面量语法（无论写布尔值还是数字，都会变成string类型） -->
       <child my-message='hello'></child>
+      <!-- v-bind动态Prop，即可以绑定实例中的数据，还有布尔值，num类型必须通过这种表达式 -->
+      <child v-bind:my-num='1'></child>
+      <child v-bind:my-other='true'></child>
+      <child v-bind:default-num='100'></child>
+      <child v-bind:default-obj='{a:1}'></child>
   </div>
 </template>
 <script>
