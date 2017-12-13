a8ec7bf4e0536bdf3630a371332f04d5f2102095
youjie <youjie@hudongpai.com>
2017-12-13 17:24:10 +0800

    提交props

---
+++ src/components/prop/child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child">
+      <p>我是子组件</p>
+      <p>{{myMessage}}</p>
+  </div>
+</template>
+<script>
+    export default {
+      name: "child",
+      props: {
+        //   子组件接收，必须以驼峰命名法
+        myMessage: String
+      }
+    };
+</script>
+
--- src/components/prop/prop.vue
+++ src/components/prop/prop.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="prop"></div>
+  <div class="prop">
+      <!-- 字面量语法，kebab-case写法 -->
+      <child my-message='hello'></child>
+  </div>
 </template>
 <script>
-    export default {};
+    //引入子组件
+    import child from "./child.vue";
+    export default {
+      components: {
+        child //注册子组件
+      }
+    };
 </script>
